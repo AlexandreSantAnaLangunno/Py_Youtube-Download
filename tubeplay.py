8f44bc1a2c5f73d739c8c3d421ce4d67da818fb9
Alexandre <71261076+AlexandreSantAnaLangunno@users.noreply.github.com>
2021-01-14 18:34:23 -0300

    Add temas

--- tubeplay.py
+++ tubeplay.py
@@ -1,36 +1,198 @@
 import pytube
-from pytube import *
-import PySimpleGUI as Pysg
+import PySimpleGUI as Pysg 
 import os
 import platform
+from pytube import YouTube
+
 
 user = os.getlogin()
 so = platform.system()
+
 if so == "Linux":
     caminho_download = "/home/"+str(user)+"/Downloads/"
 elif so == "Windows":
     caminho_download = "C:\\Users\\"+str(user)+"\\Downloads\\"
 
-Pysg.theme("DarkRed1")
 
-janela = [
-    [Pysg.Text("Link do Video:"), Pysg.Input(key = "Link")],
-    [Pysg.Text("O download será realizado em "+str(caminho_download))],
-    [Pysg.Button(button_text = "Baixar")]
-]
+Black = "Black"
+Dark = "Dark"
+Padrão = "Padrão"
+Light = "LightBlue"
 
-tela = Pysg.Window('PyTube Download', janela, element_justification="c")
+Pysg.Text()
+nav_bar=(
+    ["Temas", [Black, Dark, Light, Padrão]],
+)
 
-while True:
-    evento, valores = tela.read()
+# Janela com o Tema Padrão #
+def TemaPadrão():
+    Janela_Padrão = Pysg.theme('DarkRed1')
+    layout_padrão = [
+        [Pysg.MenuBar(nav_bar)],
+        [Pysg.Text('Link do Vídeo: '), Pysg.Input(key = 'Link')],
+        [Pysg.Text("O download será realizado em "+str(caminho_download))],
+        [Pysg.Button(button_text = "Baixar")],
+    ]
 
-    if evento == Pysg.WINDOW_CLOSED or evento == "Quit" or evento == "Cancel":
-        break
+    tela = Pysg.Window('PyTube Download', layout = layout_padrão, element_justification='c')
 
-    url = valores["Link"]
-    try:
-        Pysg.Popup("Fazendo Download...")
-        YouTube(url).streams.first().download(caminho_download)
-        Pysg.Popup("Sucesso!", "O vídeo foi baixado em "+str(caminho_download))
-    except:
-        Pysg.Popup("Ops...", "Um erro inesperado ocorreu")
+    while True:
+        evento, valores = tela.read()
+
+        url = valores['Link']
+        if evento == 'Baixar':
+            try:
+                Pysg.PopupNoTitlebar('Baixando...')
+                YouTube(url).streams.first().download(caminho_download)
+                Pysg.Popup('Sucesso! O vídeo foi baixado em: '+str(caminho_download))
+            except:
+                Pysg.Popup('Ops... ', "Um erro inesperado ocorreu.")
+
+        if evento == Pysg.WINDOW_CLOSED or evento == 'Quit':
+            break
+
+        elif evento in (Black):
+            tela.close()
+            Tema_Black()
+            break
+
+        elif evento in (Dark):
+            tela.close()
+            Tema_Dark()
+            break
+
+        elif evento in (Light):
+            tela.close()
+            Tema_Light()
+            break
+
+# Janela com o Tema Black #
+def Tema_Black():
+    Janela_Black = Pysg.theme('Black')
+    layout_Black = [
+        [Pysg.MenuBar(nav_bar)],
+        [Pysg.Text('Link do Vídeo: '), Pysg.Input(key = 'Link')],
+        [Pysg.Text("O download será realizado em "+str(caminho_download))],
+        [Pysg.Button(button_text = "Baixar")],
+        ]
+
+    tela_black = Pysg.Window('PyTube', layout = layout_Black, element_justification='c')
+
+    while True:
+        eventos, valores = tela_black.read()
+
+        url = valores['Link']
+        if eventos == 'Baixar':
+            try:
+                Pysg.PopupNoTitlebar('Baixando...')
+                YouTube(url).streams.first().download(caminho_download)
+                Pysg.Popup('Sucesso! O vídeo foi baixado em: '+str(caminho_download))
+            except:
+                Pysg.Popup('Ops... ', "Um erro inesperado ocorreu.")
+
+        if eventos == Pysg.WIN_CLOSED or eventos == 'Quit' or eventos == 'Cancel':
+            tela_black.close()
+            break
+
+        elif eventos in (Dark):
+            tela_black.close()
+            Tema_Dark()
+            break
+
+        elif eventos in (Padrão):
+            tela_black.close()
+            TemaPadrão()
+            break
+
+        elif eventos in (Light):
+            tela_black.close()
+            Tema_Light()
+            break
+
+# Janela com o Tema Dark #
+def Tema_Dark():
+    Janela_Dark = Pysg.theme('Dark')
+    layout_Dark = [
+        [Pysg.MenuBar(nav_bar)],
+        [Pysg.Text('Link do Vídeo: '), Pysg.Input(key = 'Link')],
+        [Pysg.Text("O download será realizado em "+str(caminho_download))],
+        [Pysg.Button(button_text = "Baixar")],
+    ]
+
+    tela_dark= Pysg.Window('PyTube', layout= layout_Dark, element_justification='c')
+
+    while True:
+        eventos, valores = tela_dark.read()
+
+        url = valores['Link']
+        if eventos == 'Baixar':
+            try:
+                Pysg.PopupNoTitlebar('Baixando...')
+                YouTube(url).streams.first().download(caminho_download)
+                Pysg.Popup('Sucesso! O vídeo foi baixado em: '+str(caminho_download))
+            except:
+                Pysg.Popup('Ops... ', "Um erro inesperado ocorreu.")
+
+        elif eventos in(Padrão):
+            tela_dark.close()
+            TemaPadrão()
+            break
+
+        elif eventos in (Black):
+            tela_dark.close()
+            Tema_Black()
+            break
+
+        elif eventos in (Light):
+            tela_dark.close()
+            Tema_Light()
+            break
+
+        if eventos == Pysg.WIN_CLOSED or eventos == 'Quit':
+            tela_dark.close()
+            break    
+
+# Janela com o Tema Light #
+def Tema_Light():
+    Pysg.theme('LightBlue')
+    layout_Light = [
+        [Pysg.MenuBar(nav_bar)],
+        [Pysg.Text('Link do Vídeo: '), Pysg.Input(key = 'Link')],
+        [Pysg.Text("O download será realizado em "+str(caminho_download))],
+        [Pysg.Button(button_text = "Baixar")],
+    ]
+
+    tela_Light = Pysg.Window('PyTube Download', layout= layout_Light, element_justification='c')
+
+    while True:
+        eventos, valores = tela_Light.read()
+
+        url = valores['Link']
+        if eventos == 'Baixar':
+            try:
+                Pysg.PopupNoTitlebar('Baixando...')
+                YouTube(url).streams.first().download(caminho_download)
+                Pysg.Popup('Sucesso! O vídeo foi baixado em: '+str(caminho_download))
+
+            except:
+                Pysg.Popup('Ops...', 'Um erro inesperado ocorreu.')
+
+        if eventos == Pysg.WINDOW_CLOSED or eventos == 'Quit' or eventos == 'Cancel':
+            break
+
+        elif eventos in (Black):
+            tela_Light.close()
+            Tema_Black()
+            break
+
+        elif eventos in (Dark):
+            tela_Light.close()
+            Tema_Dark()
+            break
+
+        elif eventos in (Padrão):
+            tela_Light.close()
+            TemaPadrão()
+            break
+
+TemaPadrão()
